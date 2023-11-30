90c10263a042c576b16e3fafa5a22b2b8f09b637
mis401 <62945435+mis401@users.noreply.github.com>
2023-11-30 23:48:12 +0100

    adding onto stack

--- Faza I/Logika/state.py
+++ Faza I/Logika/state.py
@@ -9,6 +9,7 @@ class Field:
         self.stack = []
         self.color = color
 
+
 class Matrix:
     def __init__(self, N):
         self.matrix = [[Field(Color.BLACK if (i+j)%2 == 0 else Color.WHITE) for i in range(N)] for j in range(N)]
@@ -22,18 +23,12 @@ class Matrix:
                 for i in range(1, self.matrix.size()-2, 2):
                     self.matrix[i][j].stack.append(Mica(Color.BLACK))
 
-def setMice(currentState, newState):
-    newMatrix = Matrix(currentState.matrix.size())
-    for item in newState:
-        newMatrix.matrix[item[0]][item[1]].stack = item[2]
-    return newMatrix
-
-
 
 class AppState:
-    def __init__(self, N):
+    def __init__(self, N, firstPlayer):
         self.matrix = Matrix(N, N)
         self.matrix.startPositions()
+        self.currentPlayer = firstPlayer
 
     def set_state(self, newState):
         self.matrix = setMice(self.matrix, newState)
@@ -41,7 +36,22 @@ class AppState:
     def get_state(self):
         return self.matrix
     
+def setMice(currentState, newState):
+    newMatrix = Matrix(currentState.matrix.size())
+    for item in newState:
+        newMatrix.matrix[item[0]][item[1]].stack = item[2]
+    return newMatrix
 
 class Color(Enum):
     BLACK=0
     WHITE=1
+
+class Player(Enum):
+    Player="player"
+    Computer="computer"
+
+
+def add(self, secondStack):
+    if(len(self.stack) + len(secondStack) > 8):
+        raise Exception("Stack overflow")
+    self.stack = self.stack + secondStack
