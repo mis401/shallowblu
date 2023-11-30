d7af25791e01c37f2136fdd16dedec732d86f869
mis401 <62945435+mis401@users.noreply.github.com>
2023-12-01 00:28:34 +0100

    izmena stacka dodatno

--- Faza I/state.py
+++ Faza I/state.py
@@ -1,5 +1,4 @@
 from enum import Enum
-
 class Mica:
     def __init__(self, color):
         self._color = color
@@ -23,20 +22,38 @@ class Matrix:
                 for i in range(1, self.matrix.size()-2, 2):
                     self.matrix[i][j].stack.append(Mica(Color.BLACK))
 
+class Score:
+    Player = 0
+    Computer = 0
+    def __init__(self):
+        self.Player = 0
+        self.Computer = 0
+
 
 class AppState:
     def __init__(self, N, firstPlayer):
         self.matrix = Matrix(N, N)
         self.matrix.startPositions()
         self.currentPlayer = firstPlayer
+        self.score = Score()
+        self.finished = False
+        #broj mica = (n-2)*n/2, max stackova je to /8, uslov za pobedu je vise od tog /2
+        self.winCondition = (N-2)*(N/2)/8/2
 
-    def set_state(self, newState):
-        self.matrix = setMice(self.matrix, newState)
+    # def set_state(self, newState):
+    #     self.matrix = setState(self.matrix, newState)
 
+
+    #valid move => (src(x, y), stackslice, dst(x, y))
+    def set_state(self, src, stackslice, dst):
+        extractedSlice = self.matrix.matrix[src[0]][src[1]].stack[stackslice:]
+        self.matrix.matrix[src[0]][src[1]].stack = self.matrix.matrix[src[0]][src[1]].stack[:stackslice]
+        self.matrix.matrix[dst[0]][dst[1]].stack = self.matrix.matrix[dst[0]][dst[1]].stack + stackslice
+        
     def get_state(self):
         return self.matrix
     
-def setMice(currentState, newState):
+def setStateImmutable(currentState, newState):
     newMatrix = Matrix(currentState.matrix.size())
     for item in newState:
         newMatrix.matrix[item[0]][item[1]].stack = [*item[2]]
@@ -55,3 +72,13 @@ def add(self, secondStack):
     if(len(self.stack) + len(secondStack) > 8):
         raise Exception("Stack overflow")
     self.stack = self.stack + secondStack
+
+def scoreIncrement(player):
+    if player == Player.Player:
+        AppState.score.Player += 1
+        if AppState.score.Player > AppState.winCondition:
+            AppState.finished = True
+    else:
+        AppState.score.Computer += 1
+        if AppState.score.Computer > AppState.winCondition:
+            AppState.finished = True
