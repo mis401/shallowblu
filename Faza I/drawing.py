232ecc06accd3a21e71924584222f93e5b98f3f1
mis401 <62945435+mis401@users.noreply.github.com>
2023-12-02 04:20:21 +0100

    doradjena dokumentacija, uredjen redosled koda

--- Faza I/drawing.py
+++ Faza I/drawing.py
@@ -16,6 +16,30 @@ screen = pygame.display.set_mode((W, H))
 tp.init(screen, tp.theme_human)  # bind screen to gui elements and set theme
 
 
+matrix_size = 0
+whos_first=""
+def my_func():
+    global matrix_size
+    global initialState
+    global appState
+    if initialState == False:
+        alert1.launch_alone()
+        alert2.launch_alone()
+    print("User has chosen:", alert1.choice)
+    try:
+        matrix_size = int(alert1.choice)
+        print(matrix_size)
+        whos_first = alert2.choice
+        print('preprint')
+        print(whos_first)
+        if appState == None and initialState == False:
+            print("usao u if")
+            appState = AppState(matrix_size, whos_first)
+            initialState = True
+    except Exception as e:
+        print(e)
+        pass
+
 def blit_before_gui():
     white_value = 127 + math.sin(iteration * math.pi * 0.5 / loop.fps) * 127
     gradient = tp.graphics.color_gradient(((0, 0, 0), (white_value, white_value, white_value)), (W, H), "v")
@@ -28,11 +52,19 @@ def blit_before_gui():
     if appState and appState.finished == True:
         winAlert = tp.Alert("Kraj igre", "Pobednik je: " + appState.currentPlayer.type.value)
         winAlert.launch_alone()
-    
-# def hardCodedStateChange():
-#     move = ((2, 0), 0, (3, 0))
-#     print(appState.matrix.matrix[3][0].stack)
-#     appState.set_state(move[0], move[1], move[2])
+
+
+def draw_chessboard(square_size=64):
+    total_width = matrix_size * square_size
+    total_height = matrix_size * square_size
+    if appState:
+        matrix = appState.matrix.matrix
+        center_x = (screen.get_width() - total_width) // 2
+        center_y = (screen.get_height() - total_height) // 2
+
+        for row in range(matrix_size):
+            for col in range(matrix_size):
+                draw_square(matrix[row][col], center_x+col*square_size, center_y+row*square_size, square_size)
 
 
 def draw_square(field, x, y, square_size):
@@ -51,8 +83,7 @@ def draw_mica(mica, x, y, width):
     mica=pygame.draw.rect(screen, color, [x, y, width, height])
     
 def selectMica(pos):
-    global selectedMica
-    global selectedSquare
+
     square_size = 64
     try:
         total_width = matrix_size * square_size
@@ -68,13 +99,8 @@ def selectMica(pos):
         upInSquare = abs(inSquare - square_size)
         print(upInSquare)
         micaPos = int(upInSquare // (square_size / 8))
-        #micaPos = ((len(selectedSquare.stack))+micaPos)%(len(selectedSquare.stack))
         print(micaPos)
         micaPos = min(micaPos, len(selectedSquare.stack)-1)
-        #micaPos = selectedSquare["square"].stack[-micaPos] if micaPos >= len(selectedSquare["square"].stack) else selectedSquare["square"].stack[len(selectedSquare["square"].stack)-1]
-        selectedMica = selectedSquare.stack[micaPos]
-        #if not selectedMica.color == appState.currentPlayer.color:
-        #    raise Exception("pogresna boja")
         appState.currentMove[0] = (selectedRow, selectedCol)
         appState.currentMove[1] = micaPos
         return True
@@ -84,9 +110,6 @@ def selectMica(pos):
         pass
 
 def selectDestSquare(pos):
-    global destSquare
-    global selectedSquare
-    global selectedMica
     square_size = 64
     try:
         total_width = matrix_size * square_size
@@ -96,79 +119,12 @@ def selectDestSquare(pos):
         top_left_y = (screen.get_height() - total_height) // 2
         selectedCol = (pos[0] - top_left_x) // square_size
         selectedRow = (pos[1] - top_left_y) // square_size
-        destSquare = matrix[selectedRow][selectedCol]
         appState.currentMove[2] = (selectedRow, selectedCol)
         return True 
     except Exception as e:
         appState.currentMove = [None, None, None]
         print(e)
         pass
-    
-
-def draw_chessboard(square_size=64):
-    total_width = matrix_size * square_size
-    total_height = matrix_size * square_size
-
-    # Calculate the position to center the chessboard
-
-
-    # 
-    
-    if appState:
-        matrix = appState.matrix.matrix
-        center_x = (screen.get_width() - total_width) // 2
-        center_y = (screen.get_height() - total_height) // 2
-
-        for row in range(matrix_size):
-            for col in range(matrix_size):
-                draw_square(matrix[row][col], center_x+col*square_size, center_y+row*square_size, square_size)
-
-
-choices_whos_first = ("player", "computer")
-whos_first_text = "Ko prvi igra?"
-alert2 = tp.AlertWithChoices("ko prvi igra?", choices_whos_first, whos_first_text, choice_mode="h")
-choices_matix = ("8", "10", "12", "14", "16")
-matrix_text = "Koju zelite dimeziju?"
-alert1 = tp.AlertWithChoices("Grid dimenzija", choices_matix, matrix_text, choice_mode="v")
-
-
-matrix_size = 0
-whos_first=""
-def my_func():
-    global matrix_size  # Use global to modify the outer variable
-    global initialState
-    global appState
-    if initialState == False:
-        alert1.launch_alone()
-        alert2.launch_alone()
-    print("User has chosen:", alert1.choice)
-    try:
-        matrix_size = int(alert1.choice)
-        print(matrix_size)
-        whos_first = alert2.choice
-        print('preprint')
-        print(whos_first)
-        if appState == None and initialState == False:
-            print("usao u if")
-            appState = AppState(matrix_size, whos_first)
-            initialState = True
-        
-
-    except Exception as e:
-        print(e)
-        pass
-launcher = tp.Button("Odaberi velicinu table")
-launcher.set_topleft(10, 10)
-launcher.at_unclick = my_func
-launcher2= tp.Button("Ko igra prvi?")
-launcher2.set_topleft(10, 50)
-
-
-
-loop = launcher.get_updater(fps=60)
-clock = pygame.time.Clock()
-iteration = 0
-
 
 def performMove():
     startPos = appState.currentMove[0]
@@ -189,7 +145,8 @@ def checkMove(startPos, sliceIndex, destPos):
     validity = False
     if dx == 1 and dy == 1:
         validity = True
-    if len(appState.matrix.matrix[destPos[0]][destPos[1]].stack) + len(appState.matrix.matrix[startPos[0]][startPos[1]].stack[sliceIndex:]) > 8:
+    if len(appState.matrix.matrix[destPos[0]][destPos[1]].stack) + \
+        len(appState.matrix.matrix[startPos[0]][startPos[1]].stack[sliceIndex:]) > 8:
         validity = False
     return validity
 
@@ -198,6 +155,25 @@ def aiMove():
     if appState:
         appState.switchPlayer()
     return
+
+
+choices_whos_first = ("player", "computer")
+whos_first_text = "Ko prvi igra?"
+alert2 = tp.AlertWithChoices("ko prvi igra?", choices_whos_first, whos_first_text, choice_mode="h")
+choices_matix = ("8", "10", "12", "14", "16")
+matrix_text = "Koju zelite dimeziju?"
+alert1 = tp.AlertWithChoices("Grid dimenzija", choices_matix, matrix_text, choice_mode="v")
+
+launcher = tp.Button("Odaberi velicinu table")
+launcher.set_topleft(10, 10)
+launcher.at_unclick = my_func
+launcher2= tp.Button("Ko igra prvi?")
+launcher2.set_topleft(10, 50)
+
+loop = launcher.get_updater(fps=60)
+clock = pygame.time.Clock()
+iteration = 0
+
 
 
 while loop.playing:
--- Faza I/state.py
+++ Faza I/state.py
@@ -1,19 +1,22 @@
 from enum import Enum
+#klasa za svaku Micu
 class Mica:
     def __init__(self, color):
         self.color : Color = color
 
+#klasa za svako polje na tabli
 class Field:
     def __init__(self, color):
         print("creating field")
         self.stack: Mica = []
         self.color : Color = color
 
-
+#klasa za tablu
 class Matrix:
     def __init__(self, N):
         print("creating matrix")
-        self.matrix : Field = [[Field(Color.BLACK if (i+j)%2 == 0 else Color.WHITE) for i in range(N)] for j in range(N)]
+        self.matrix : Field = [[Field(Color.BLACK if (i+j)%2 == 0 else Color.WHITE) for i in range(N)] \
+                               for j in range(N)]
 
     def startPositions(self):
         for j in range(0, len(self.matrix)):
@@ -24,19 +27,30 @@ class Matrix:
                 for i in range(1, len(self.matrix)-2, 2):
                     self.matrix[i][j].stack.append(Mica(Color.BLACK))
 
+#klasa za vodjenje stanja poena
 class Score:
     def __init__(self):
         self.PlayerOne = 0
         self.PlayerTwo = 0
 
+#klasa za igraca
+class Player:
+    def __init__(self, typeOfPlayer, color):
+        self.type : PlayerType = typeOfPlayer
+        self.color : Color = color
 
+
+#klasa za celokupno stanje aplikacije
 class AppState:
     def __init__(self, N, firstPlayer):
         print("creating appstate")
         print(firstPlayer == PlayerType.Player.value)
         self.matrix = Matrix(N)
         self.matrix.startPositions()
-        self.players = [Player(PlayerType.Player, Color.WHITE if firstPlayer==PlayerType.Player.value else Color.BLACK), Player(PlayerType.Computer, Color.WHITE if firstPlayer == PlayerType.Computer.value else Color.WHITE)]
+        self.players = [Player(PlayerType.Player, \
+                               Color.WHITE if firstPlayer==PlayerType.Player.value else Color.BLACK), \
+                                Player(PlayerType.Computer, Color.WHITE if firstPlayer == PlayerType.Computer.value \
+                                else Color.WHITE)]
         self.currentPlayer = self.players[0] if self.players[0].color == Color.WHITE else self.players[1]
         self.score = Score()
         self.finished = False
@@ -44,11 +58,16 @@ class AppState:
         self.winCondition = (N-2)*(N/2)/8/2
         self.currentMove = [None, None, None]
 
-    # def set_state(self, newState):
-    #     self.matrix = setState(self.matrix, newState)
-    def switchPlayer(self):
-        print("switching players")
-        self.currentPlayer = self.players[1] if self.currentPlayer == self.players[0] else self.players[0]
+    #valid move => (src(x, y), stackslice, dst(x, y))
+    def set_state(self, src, stackslice, dst):
+        extractedSlice = self.matrix.matrix[src[0]][src[1]].stack[stackslice:]
+        self.matrix.matrix[src[0]][src[1]].stack = self.matrix.matrix[src[0]][src[1]].stack[:stackslice]
+        self.matrix.matrix[dst[0]][dst[1]].stack = self.matrix.matrix[dst[0]][dst[1]].stack + extractedSlice
+        if len(self.matrix.matrix[dst[0]][dst[1]].stack) == 8:
+            self.scoreIncrement(self.matrix.matrix[dst[0]][dst[1]].stack[7].color)
+            self.matrix.matrix[dst[0]][dst[1]].stack.clear()
+        if self.finished==False:
+            self.switchPlayer()
 
     def scoreIncrement(self, color):
         print("inkrementira se skor za " + color.name)
@@ -63,49 +82,20 @@ class AppState:
         print(self.score.PlayerOne)
         print(self.score.PlayerTwo)
 
-    #valid move => (src(x, y), stackslice, dst(x, y))
-    def set_state(self, src, stackslice, dst):
-        extractedSlice = self.matrix.matrix[src[0]][src[1]].stack[stackslice:]
-        print("Menja se stanje:")
-        self.matrix.matrix[src[0]][src[1]].stack = self.matrix.matrix[src[0]][src[1]].stack[:stackslice]
-        self.matrix.matrix[dst[0]][dst[1]].stack = self.matrix.matrix[dst[0]][dst[1]].stack + extractedSlice
-        if len(self.matrix.matrix[dst[0]][dst[1]].stack) > 8:
-            self.currentMove = [None, None, None]
-            raise Exception("Stack overflow")
-        if len(self.matrix.matrix[dst[0]][dst[1]].stack) == 8:
-            self.scoreIncrement(self.matrix.matrix[dst[0]][dst[1]].stack[7].color)
-            self.matrix.matrix[dst[0]][dst[1]].stack.clear()
-        if self.finished==False:
-            self.switchPlayer()
+            
+    def switchPlayer(self):
+        print("switching players")
+        self.currentPlayer = self.players[1] if self.currentPlayer == self.players[0] else self.players[0]
+
         
-    def get_state(self):
-        return self.matrix
-    
-
-
-
-    
-def setStateImmutable(currentState, newState):
-    newMatrix = Matrix(len(currentState.matrix))
-    for item in newState:
-        newMatrix.matrix[item[0]][item[1]].stack = [*item[2]]
-    return newMatrix
 
 class Color(Enum):
     BLACK=0
     WHITE=1
 
+
 class PlayerType(Enum):
     Player="player"
     Computer="computer"
     
-class Player:
-    def __init__(self, typeOfPlayer, color):
-        self.type : PlayerType = typeOfPlayer
-        self.color : Color = color
-
-def add(self, secondStack):
-    if(len(self.stack) + len(secondStack) > 8):
-        raise Exception("Stack overflow")
-    self.stack = self.stack + secondStack
 
